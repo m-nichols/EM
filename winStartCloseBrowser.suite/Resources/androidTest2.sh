8e8297c123b67b79011204bdbbc3ccac9eff5519
m-nichols <m.nichols_64@yahoo.com>
2017-09-15 09:30:36 -0600

    Add files via upload

---
+++ winStartCloseBrowser.suite/Resources/androidTest2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while [ 1 ];
+do
+"/Applications/eggPlant.app/runscript" "~/Desktop/Samsung.suite/scripts/pressreleasehold.script" -host "SAMSUNG-SGH-I747 (1d088533)" -port 5902
+sleep 3
+done
